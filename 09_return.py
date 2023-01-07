c80de49b93a0f42667e28f93df40026543e7537f
Rene Mejia <rene.mejiac@gmail.com>
2023-01-07 13:42:33 -0800

    - Default parameters and multiple returns - comments

--- 09_return.py
+++ 09_return.py
@@ -9,10 +9,12 @@ print(f"find_volume(width=10) => {find_volume(width=10)}")
 
 print("-" * 25)
 
-def find_volume(length=1, width=1, depth=1): # length=1 - default parameter: 1.
-    return length * width * depth, width, 'hola'
+def find_volume(length=1, width=1, depth=1): # Default parameter: 1.
+    return length * width * depth, width, 'hola' # Multiple Returns
+
+# Handling multiple returns
 
 result, width, text = find_volume(width=10)
 print(result)
 print(width)
-print(text)
+print(text) 
