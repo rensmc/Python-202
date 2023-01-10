9ea846d599071aff8bf976946145f6317f495b2f
Rene Mejia <rene.mejiac@gmail.com>
2023-01-09 17:47:02 -0800

    + Immutability

---
+++ 16_inmutable.py
@@ -0,0 +1,30 @@
+# Maps using dictionaries
+
+items = [
+    {
+        'product': 'shirt',
+        'price': 100
+    },
+    {
+        'product': 'pants',
+        'price': 300
+    },
+    {
+        'product': 'trousers',
+        'price': 900
+    },
+]
+
+prices = list(map(lambda item: item['price'], items))
+print(prices)
+
+# Adding taxes
+
+def add_taxes(item):
+    new_item = item.copy()
+    new_item['taxes'] = new_item['price'] * .19
+    return new_item
+
+new_items = list(map(add_taxes, items)) # Creating a new list with taxes
+print(new_items)
+print(items)
