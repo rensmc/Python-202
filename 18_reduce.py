58fdb919d8acf9bbfad38b28b539b19d589b982c
Rene Mejia <rene.mejiac@gmail.com>
2023-01-11 13:36:55 -0800

    + Reduce

---
+++ 18_reduce.py
@@ -0,0 +1,11 @@
+import functools
+
+numbers = [1, 2, 3, 4]
+
+def accum(counter, item):
+    print('counter => ', counter)
+    print('item => ', item)
+    return counter + item
+
+result = functools.reduce(accum, numbers)
+print(result)
