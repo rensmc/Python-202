cc47a2e1c53fd7fe5586bde9fdfaef5a3c438331
Rene Mejia <rene.mejiac@gmail.com>
2023-01-12 09:04:13 -0800

    + Python modules

---
+++ 19_modules.py
@@ -0,0 +1,20 @@
+import sys # Allows us to know about the state of the OS
+print(sys.path[0])
+
+import re # Regular expressions
+text = "My phone number is 555 555555, country code is 55 and my lucky number is also 5"
+result = re.findall("[0-9]+", text) # Find every number in the string.
+print(result)
+
+import time # Date and time
+timestamp = time.time() # Time in Unix format
+print(timestamp)
+
+local = time.localtime() # Local time
+result = time.asctime(local) # Formats the date
+print(result)
+
+import collections # Mainly for managing lists
+numbers = [1, 1, 2, 1, 2, 1, 4, 5, 3, 3, 21]
+counter = collections.counter(numbers) # Creates a dictionary with the frecuency of each number
+print(counter) 
