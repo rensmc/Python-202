7b0acf350f9ddb1d7ee2ec7a9c4a5704906317ca
Rene Mejia <rene.mejiac@gmail.com>
2023-01-18 17:00:04 -0800

    + Modules as scripts - __name__ and __main__

--- app/main.py
+++ app/main.py
@@ -1,21 +1,26 @@
 import utils
 
-keys, values = utils.get_population()
-print(keys,values)
+def run():
+    keys, values = utils.get_population()
+    print(keys,values)
 
-data = [
-    {
-        'Country': 'Colombia',
-        'Population': 500
-    },
-    {
-        'Country': 'Bolivia',
-        'Population': 300
-    }
-]
+    data = [
+        {
+            'Country': 'Colombia',
+            'Population': 500
+        },
+        {
+            'Country': 'Bolivia',
+            'Population': 300
+        }
+    ]
 
-country = input('Type country => ')
-result = utils.population_by_country(data, country)
-print(result)
+    country = input('Type country => ')
+    result = utils.population_by_country(data, country)
+    print(result)
 
-print(utils.a)
+    print(utils.a)
+
+# Allow file to be executed as a script.
+if __name__ == '__main__': 
+    run()
