38e1c47f4c5a101e396fcd73e78d2433ec665c0c
Rene Mejia <rene.mejiac@gmail.com>
2023-01-19 20:59:58 -0800

    + Manual iterations with iter()

---
+++ 20_iter.py
@@ -0,0 +1,17 @@
+for i in range(1,11):
+    print(i)
+
+my_iter = iter(range(1, 11))
+print(my_iter)
+
+# Manually iterating the for loop, step by step.
+print(next(my_iter)) 
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
